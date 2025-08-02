e388761b7ae5346262f0ad7ac2bd9b9c97efca0c
Matheus Martins <matheushmar@gmail.com>
2025-08-01 22:19:11 -0300

    Add Products page and update application structure
    - Introduced Products component with routing and basic template.
    - Updated app routing to include Products page.
    - Added Less CSS preprocessor dependency.
    - Replaced logo in sidebar and updated application title.
    - Added favicon and logo assets.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,5 +2,6 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: '/welcome' },
-  { path: 'welcome', loadChildren: () => import('./pages/welcome/welcome.routes').then(m => m.WELCOME_ROUTES) }
+  { path: 'welcome', loadChildren: () => import('./pages/welcome/welcome.routes').then(m => m.WELCOME_ROUTES) },
+  { path: 'products', loadChildren: () => import('./pages/products/products.routes').then(m => m.PRODUCTS_ROUTES) }
 ];
---
+++ src/app/pages/products/products.routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from '@angular/router';
+import { Products } from './products';
+
+export const PRODUCTS_ROUTES: Routes = [
+  { path: '', component: Products },
+];
