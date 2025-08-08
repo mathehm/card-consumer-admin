7ae313207fe2b53a6e350086cef3cd9ef45f896c
Matheus Martins <matheushmar@gmail.com>
2025-08-08 00:48:34 -0300

    feat: remove withFetch from HTTP client configuration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -9,7 +9,7 @@ import { registerLocaleData } from '@angular/common';
 import pt from '@angular/common/locales/pt';
 import { FormsModule } from '@angular/forms';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { provideHttpClient, withFetch } from '@angular/common/http';
+import { provideHttpClient } from '@angular/common/http';
 
 registerLocaleData(pt);
 
@@ -17,6 +17,6 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes), provideNzIcons(icons), provideNzI18n(pt_BR), importProvidersFrom(FormsModule), provideAnimationsAsync(), provideHttpClient(withFetch())
+    provideRouter(routes), provideNzIcons(icons), provideNzI18n(pt_BR), importProvidersFrom(FormsModule), provideAnimationsAsync(), provideHttpClient()
   ]
 };
